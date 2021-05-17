2cc93ea20895f0108f4b6875232aa3c307354b37
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-05-17 15:26:15 +0200

    added test class

---
+++ src/dennis/com/ap2/dennis/com/ap2/NodeTest.java
@@ -0,0 +1,34 @@
+package dennis.com.ap2;
+
+import static org.junit.Assert.*;
+
+public class NodeTest {
+
+    @org.junit.Test
+    public void getCode() {
+    }
+
+    @org.junit.Test
+    public void setCode() {
+    }
+
+    @org.junit.Test
+    public void setCurrentnode() {
+    }
+
+    @org.junit.Test
+    public void getCurrentnode() {
+    }
+
+    @org.junit.Test
+    public void setNaam() {
+    }
+
+    @org.junit.Test
+    public void getNaam() {
+    }
+
+    @org.junit.Test
+    public void FSM() {
+    }
+}
